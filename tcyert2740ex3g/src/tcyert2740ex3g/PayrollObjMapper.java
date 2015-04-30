3a37a7f3e25f72f6c14fcf2a4b70eb78b6204b83
Tyler Cyert <ci6965ka@webmail.southeastmn.edu>
2015-04-29 22:40:34 -0500

    Initial Commit

---
+++ tcyert2740ex3g/src/tcyert2740ex3g/PayrollObjMapper.java
@@ -0,0 +1,140 @@
+package tcyert2740ex3g;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+import javax.swing.DefaultListModel;
+
+public class PayrollObjMapper {
+
+	private String fileName;
+	private PrintWriter outputFile;
+	private	Scanner inputFile;
+	private File file;
+	
+	public PayrollObjMapper(String fileName) {
+		super();
+		this.fileName = fileName;
+	}
+	
+	public boolean openInputFile() {
+		boolean fileOpened = false;
+		
+//open file
+	    try {
+	    	File file = new File(this.fileName);
+			fileOpened = file.exists();
+			if (fileOpened) {
+				// Open the file.
+			    this.inputFile = new Scanner(file);
+
+			}
+	    }
+	    catch (IOException e) {}
+	    
+		return fileOpened;
+	} 
+	
+	public boolean openOutputFile() {
+		boolean fileOpened = false;
+		try {
+			outputFile = new PrintWriter(fileName);
+			fileOpened = true;
+		}
+		catch(IOException e){}
+		
+		return fileOpened ;
+	
+	}
+	
+	public void closeInputFile() {
+		if (this.inputFile != null) this.inputFile.close();
+	}
+	
+	public void closeOutputFile() {
+		if(outputFile != null) outputFile.close();
+	}
+	
+	public Payroll getNextPayroll() {
+		Payroll p = null;
+		
+//		String textLine = inputFile.nextLine();
+//		int id = Integer.parseInt(textLine);
+//		String name = inputFile.nextLine();
+//		textLine = inputFile.nextLine();
+//		double payRate = Double.parseDouble(textLine);
+//		textLine = inputFile.nextLine();
+//		double hours = Double.parseDouble(textLine);
+		
+		int id = 0;
+		String name = "";
+		Double payRate = 0.0;
+		double hours = 0.0;
+		
+		try {
+			String textLine = inputFile.nextLine();
+			id = Integer.parseInt(textLine);
+			name = inputFile.nextLine();
+			textLine = inputFile.nextLine();
+			payRate = Double.parseDouble(textLine);
+			textLine = inputFile.nextLine();
+			hours = Double.parseDouble(textLine);
+			p = new Payroll(id, name, payRate, hours);
+		}
+		catch (NoSuchElementException e){ }
+		catch (NumberFormatException e) { }
+		
+		
+		
+		return p;
+	}
+	
+	public void writePayroll(Payroll payroll) {
+		if (this.outputFile != null && payroll != null) {
+			outputFile.println(payroll.getId());
+			outputFile.println(payroll.getName());
+			outputFile.println(payroll.getPayRate());
+			outputFile.println(payroll.getHours());
+		}
+	}
+	
+	public DefaultListModel getAllPayroll() {
+		DefaultListModel payrollCollection = new DefaultListModel();
+	
+		if (this.openInputFile()) {
+			
+			while (this.inputFile.hasNextLine()){
+				
+				Payroll p = this.getNextPayroll();
+				if (p != null) {
+					payrollCollection.addElement(p);
+				}
+			}
+		}
+		
+		this.closeInputFile();
+		return payrollCollection;
+		
+	}
+	
+	public void writeAllPayroll(DefaultListModel payrollCollection) {
+		if (this.openOutputFile()) {
+			int size = (payrollCollection.getSize() - 1);
+			for (int i = 0; i <= size;) {
+				Payroll p = (Payroll) payrollCollection.get(i);
+				this.writePayroll(p);	
+				i++;
+	//			p = (Payroll) payrollCollection.get(1);
+	//			this.writePayroll(p);	
+	//			p = (Payroll) payrollCollection.get(2); 
+	//			this.writePayroll(p);	
+			}
+			
+		}
+		this.closeOutputFile();
+	
+	}
+}
